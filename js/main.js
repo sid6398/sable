f7f620da94685343dd0ceb529e3175f37718a85b
Siddhesh Mithbavkar <siddheshmithbavkar98@gmail.com>
2025-03-26 00:26:35 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,49 @@
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 1000,
+        disableOnInteraction: false,
+    },
+    slidesPerView: "auto",
+    spaceBetween: 48,
+});
+
+
+
+
+gsap.to(".card1", {
+    rotationY: 25,   // More noticeable tilt
+    rotationX: 15,
+    y: -20,          // More floating height
+    duration: 2.5,   // Slow & smooth
+    repeat: -1,
+    yoyo: true,
+    ease: "power1.inOut"
+});
+
+
+gsap.to(".card2", {
+    rotationY: 360,
+    duration: 5,
+    repeat: -1,
+    ease: "linear"
+});
+
+gsap.to(".card3", {
+    x: "80vw",
+    rotationY: 360, 
+    rotationX: 180,
+    duration: 4, 
+    repeat: -1, 
+    yoyo: true, 
+    ease: "power1.inOut"
+  });
+  
+  
+  
+  
+  
